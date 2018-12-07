96ea1c893d74197db76322846c1a0399aa88c3d9
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-07 05:50:41 +0100

    Added auto-reply with the WIZ logo

--- src/main/java/bot_wrapper/MathBot.java
+++ src/main/java/bot_wrapper/MathBot.java
@@ -1,5 +1,10 @@
 package bot_wrapper;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -7,14 +12,20 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MathBot extends TelegramLongPollingBot {
+  protected String logoPath = "./generated-sources/banner.txt";
+
   @Override
   public void onUpdateReceived(Update update) {
     if (update.hasMessage() && update.getMessage().hasText()) {
       Message inp = update.getMessage();
-      System.out.println(inp.getText());
       SendMessage msg = new SendMessage()
-          .setChatId(inp.getChatId())
-          .setText(inp.getText());
+          .setChatId(inp.getChatId());
+      try {
+        List<String> wiz_logo = getLogo(logoPath);
+        msg.setText(String.valueOf((Object) wiz_logo));
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
       try {
         execute(msg);
       } catch (TelegramApiException e) {
@@ -29,5 +40,8 @@ public class MathBot extends TelegramLongPollingBot {
   @Override
   public String getBotToken () {
     return System.getenv().get("BOT_ACCESS_TOKEN");
+  }
+  public static List<String> getLogo(String filename) throws IOException {
+    return Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
   }
 }
