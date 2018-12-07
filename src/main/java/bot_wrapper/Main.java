b1c16358801a412030a5801477403b140ab69386
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-07 04:04:47 +0100

    It should work NOW

--- src/main/java/bot_wrapper/Main.java
+++ src/main/java/bot_wrapper/Main.java
@@ -1,7 +1,7 @@
 package bot_wrapper;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-// import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 public class Main {
@@ -10,11 +10,10 @@ public class Main {
 
     TelegramBotsApi botsApi = new TelegramBotsApi();
 
-    System.out.print("EVERYTHING SEEMS TO BE ALRIGHT AT THIS MOMENT!");
-    // try {
-    //   botsApi.registerBot(new MathBot());
-    // } catch (TelegramApiRequestException e) {
-    //   e.printStackTrace();
-    // }
+    try {
+      botsApi.registerBot(new MathBot());
+    } catch (TelegramApiRequestException e) {
+      e.printStackTrace();
+    }
   }
 }
