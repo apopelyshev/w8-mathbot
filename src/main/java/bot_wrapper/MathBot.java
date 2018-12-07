f40747222b00032c0e9055aa6e59b4aace545b7d
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-07 06:04:43 +0100

    Trying to get right text file

--- src/main/java/bot_wrapper/MathBot.java
+++ src/main/java/bot_wrapper/MathBot.java
@@ -12,7 +12,12 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MathBot extends TelegramLongPollingBot {
-  protected String logoPath = "./generated-sources/banner.txt";
+  protected String[] logoPaths = {
+    "../target/generated-sources/banner.txt",
+    "/generated-sources/banner.txt",
+    "generated-sources/banner.txt",
+    "../generated-sources/banner.txt"
+  };
 
   @Override
   public void onUpdateReceived(Update update) {
@@ -20,11 +25,17 @@ public class MathBot extends TelegramLongPollingBot {
       Message inp = update.getMessage();
       SendMessage msg = new SendMessage()
           .setChatId(inp.getChatId());
-      try {
-        List<String> wiz_logo = getLogo(logoPath);
-        msg.setText(String.valueOf((Object) wiz_logo));
-      } catch (IOException e1) {
-        e1.printStackTrace();
+      for (String path : logoPaths) {
+        try {
+          List<String> wiz_logo = getLogo(path);
+          String res = String.valueOf((Object) wiz_logo);
+          if (res!=null && res!="") {
+            msg.setText(res);
+            break;
+          }
+        } catch (IOException e1) {
+          continue;
+        }
       }
       try {
         execute(msg);
