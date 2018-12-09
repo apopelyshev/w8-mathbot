2307403edb9c3034c990d8f41dd2cffa3ce2e310
Artem Popelyshev <apopelyshev@gmail.com>
2018-12-09 21:18:58 +0100

    Try #11 with Tomcat

--- src/main/java/bot_wrapper/MathBot.java
+++ src/main/java/bot_wrapper/MathBot.java
@@ -5,8 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
-import org.apache.http.message.BufferedHeader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -14,21 +14,27 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MathBot extends TelegramLongPollingBot {
-  protected static final String DEBUG_STUB_PATH = "/app/target/classes/banner.txt";
+  protected final String DEBUG_STUB_PATH = "/app/target/classes/banner.txt";
 
   @Override
   public void onUpdateReceived(Update update) {
     if (update.hasMessage() && update.getMessage().hasText()) {
       Message inp = update.getMessage();
+
       SendMessage msg = new SendMessage()
-          .setChatId(inp.getChatId());
-      msg.setText(getDebugStub())
-         .enableHtml(true);
+         .setChatId(inp.getChatId());
+      String path = MathBot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
       try {
+        String decodedPath = URLDecoder.decode(path, "UTF-8");
+        msg.setText(decodedPath);
         execute(msg);
+      } catch (UnsupportedEncodingException e1) {
+        e1.printStackTrace();
       } catch (TelegramApiException e) {
         e.printStackTrace();
       }
+      // msg.setText(this.getDebugStub())
+      //    .enableHtml(true);
     }
   }
   @Override
@@ -36,19 +42,21 @@ public class MathBot extends TelegramLongPollingBot {
     return "w8_mathbot";
   }
   @Override
-  public String getBotToken () {
+  public String getBotToken() {
     return System.getenv().get("BOT_ACCESS_TOKEN");
   }
-  public static String getDebugStub() {
+  public String getDebugStub() {
     File file = new File(DEBUG_STUB_PATH);
     try {
       FileReader reader = new FileReader(file);
       BufferedReader bfReader = new BufferedReader(reader);
       StringBuffer bfString = new StringBuffer();
       String line;
+
       while ((line = bfReader.readLine())!=null)
         bfString.append(line+"\n");
       reader.close();
+
       return bfString.toString();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
